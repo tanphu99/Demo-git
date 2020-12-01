8cd5e046c1e6e7fc204cff387db05534d27d2924
Tan Phu <tanphu@gmail.com>
2020-12-01 08:27:35 +0700

    Add function die for Moive

--- Moive.js
+++ Moive.js
@@ -1,3 +1,9 @@
-const movie = () => {};
+const movie = (name) => {
+  this.name = name;
+  this.dead = false;
+};
 
+movie.prototype.die = function () {
+  this.dead = true;
+};
 module.export = movie;
